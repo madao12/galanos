9ba4dee33fb1d4fd460b8390ea4cb6fbae211519
unknown <lefteris4g@gmail.com>
2018-09-05 16:02:33 +0300

    sww

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'galanos', component: HomeComponent }
+  { path: 'galanos', component: NavBarComponent  }
 ];
 
 @NgModule({
